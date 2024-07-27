86a9bfed6d2dcfc28c12d95e57c591e91958b0f6
Leyenda0959 <97137761+Leyenda0959@users.noreply.github.com>
2024-07-27 14:32:44 -0600

    Update guildMemeber.js

--- events/guildMemeber.js
+++ events/guildMemeber.js
@@ -1,7 +1,5 @@
-const { Events } = require('discord.js');
-
 module.exports = {
-  name: Events.GuildMemberAdd,
+  name: 'guildMemberAdd',
   async execute(member) {
     try {
       // Cambia el nombre del usuario a "Anonymous"
@@ -15,6 +13,7 @@ module.exports = {
 
       // Enviar mensaje privado de bienvenida y notificación de cambio de nombre
       await member.send('```Bienvenido al servidor. Tu nombre ha sido cambiado a ***"Anonymous"*** para proteger tu privacidad.``` ***para doble capa de seguridad manda mensaje al dm al bot y se enviara al canal general***               *** !! Si envías mensaje directamente algun canal del servidor se mostrara tu nombre por un breve periodo de tiempo !!!***');
+        
     } catch (error) {
       console.error('Error al manejar la incorporación del nuevo miembro:', error);
       const errorChannelId = '1262219587529871413'; // Reemplaza con el ID de tu canal de errores
