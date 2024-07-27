11f2603e56385b0bdca7dd2602f60c376cecde7b
Leyenda0959 <97137761+Leyenda0959@users.noreply.github.com>
2024-07-26 22:41:19 -0600

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,1 +1,10 @@
+module.exports = {
+  name: 'say',
+  description: 'Send an anonymous message',
+  execute(message, args) {
+    const content = args.join(' ');
+    message.delete(); 
+    message.channel.send(content);
+  }
+};
 
