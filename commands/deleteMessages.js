1d0980eae7d5f108ae1f1f7f7da141c8c3aee3ef
Leyenda0959 <97137761+Leyenda0959@users.noreply.github.com>
2024-07-27 19:40:04 -0600

    Update deleteMessages.js

--- commands/deleteMessages.js
+++ commands/deleteMessages.js
@@ -1,40 +1,50 @@
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-  name: 'delmessage',
-  description: 'Delete all messages of a specified user in the server',
+  name: 'clear',
+  description: 'Borra todos los mensajes de un usuario en todo el servidor.',
   async execute(message, args) {
     if (!message.member.permissions.has('MANAGE_MESSAGES')) {
-      return message.reply('No tienes permiso para usar este comando.');
+      return message.reply('No tienes permisos para usar este comando.');
     }
 
     const user = message.mentions.users.first();
     if (!user) {
-      return message.reply('Por favor menciona a un usuario.');
+      return message.reply('Debes mencionar a un usuario para borrar sus mensajes.');
     }
 
-    let deletedCount = 0;
+    const fetchMessages = async (channel) => {
+      let messages;
+      let messageCount = 0;
+      let lastMessageId = null;
 
-    const channels = message.guild.channels.cache.filter(channel => channel.isText());
-    for (const channel of channels.values()) {
-      let fetchedMessages;
       do {
-        fetchedMessages = await channel.messages.fetch({ limit: 100 });
-        const userMessages = fetchedMessages.filter(msg => msg.author.id === user.id);
+        messages = await channel.messages.fetch({ limit: 100, before: lastMessageId });
+        const userMessages = messages.filter(msg => msg.author.id === user.id);
 
-        for (const msg of userMessages.values()) {
+        for (const [msgId, msg] of userMessages) {
           await msg.delete();
-          deletedCount++;
         }
-      } while (fetchedMessages.size >= 100);
+
+        messageCount += userMessages.size;
+        lastMessageId = messages.last() ? messages.last().id : null;
+      } while (messages.size === 100);
+
+      return messageCount;
+    };
+
+    let totalDeleted = 0;
+    const channels = message.guild.channels.cache.filter(channel => channel.isText());
+
+    for (const [channelId, channel] of channels) {
+      totalDeleted += await fetchMessages(channel);
     }
 
     const embed = new MessageEmbed()
-      .setColor('#FF0000')
-      .setTitle('Mensajes eliminados')
-      .setDescription(`Se eliminaron ${deletedCount} mensajes de @${user.tag}.`)
-      .setTimestamp();
+      .setTitle('Limpieza de Mensajes')
+      .setDescription(`Se han borrado ${totalDeleted} mensajes de ${user.tag}.`)
+      .setColor('#FF0000');
 
     message.channel.send({ embeds: [embed] });
-  }
+  },
 };
